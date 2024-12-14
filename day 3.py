c8891a485cd0da0099481ea4cebf25e8067e6deb
CHITTAVADAGI-CRISTIN-PRATHEEK <cristinpratheek0@gmail.com>
2024-12-14 09:54:02 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,4 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 16:30:00) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Users\jasmine pravallika\Desktop\day 3.py ============
